5829d7c04157326278ba16817625a8241e4da640
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-17 15:02:35 -0400

    cleanup; documentation

--- VCF_prepper.sh
+++ VCF_prepper.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-
-#	$1	the reference genome
-#	$2
+#############################################
+#	$1	the reference genome				#
+#	$2 										#
 #	$3	the FASTQ files of sequences to align
-#	$4	job title
-
+#	$4	job title							#
 #############################################
 Load relevant modules 						#
 . /nas02/apps/Modules/default/init/bash		#
-module load bwa
-module load samtools
-#	Is the reference genome ready?
-
-###	Align the reads!
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $4_1.sai
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $4_2.sai
-bwa sampe $1 read1.sai read2.sai $2 $3 > $4.sam
-###	Cleanup the alignment
-samtools view -bS $4.sam > $4.bam
-samtools sort $4.bam $4.sort
-samtools index $4.sort.bam
-###	Generate the VCF
+module load bwa								#
+module load samtools						#
+#	Is the reference genome ready?			#
+###	Align the reads! ########################################
+bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $4_1.sai	#	Parameters from PSISeq procedure
+bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $4_2.sai	#
+bwa sampe $1 read1.sai read2.sai $2 $3 > $4.sam 			#
+###	Cleanup the alignment	#################################
+samtools view -bS $4.sam > $4.bam							#
+samtools sort $4.bam $4.sort								#
+samtools index $4.sort.bam									#
+###	Generate the VCF ########################################
 samtools mpileup -uf $1 $4.sort.bam | bcftools view -bvcg - > $4.bcf
-bcftools view $4.bcf | vcfutils.pl varFilter -D100 > $4.vcf
-###	Generate the .SNPS file
-python SNP_call.py $4.vcf $4.sort.bam $4.SNPS
-
+bcftools view $4.bcf | vcfutils.pl varFilter -D100 > $4.vcf #	http://samtools.sourceforge.net/mpileup.shtml
+###	Generate the .SNPS file #################################
+python SNP_call.py $4.vcf $4.sort.bam $4.SNPS				#
+#############################################################
