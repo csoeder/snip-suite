3a80c7706553b9acc77c61afa3d2ede699aa9e48
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-17 14:50:53 -0400

    5 ---> 4

--- VCF_prepper.sh
+++ VCF_prepper.sh
@@ -3,8 +3,7 @@
 #	$1	the reference genome
 #	$2
 #	$3	the FASTQ files of sequences to align
-#	$4	the VCF output
-#	$5	job title
+#	$4	job title
 
 #############################################
 Load relevant modules 						#
@@ -14,16 +13,16 @@ module load samtools
 #	Is the reference genome ready?
 
 ###	Align the reads!
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $5_1.sai
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $5_2.sai
-bwa sampe $1 read1.sai read2.sai $2 $3 > $5.sam
+bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $4_1.sai
+bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $4_2.sai
+bwa sampe $1 read1.sai read2.sai $2 $3 > $4.sam
 ###	Cleanup the alignment
-samtools view -bS $5.sam > $5.bam
-samtools sort $5.bam $5.sort
-samtools index $5.sort.bam
+samtools view -bS $4.sam > $4.bam
+samtools sort $4.bam $4.sort
+samtools index $4.sort.bam
 ###	Generate the VCF
-samtools mpileup -uf $1 $5.sort.bam | bcftools view -bvcg - > $5.bcf
-bcftools view $5.bcf | vcfutils.pl varFilter -D100 > $5.vcf
+samtools mpileup -uf $1 $4.sort.bam | bcftools view -bvcg - > $4.bcf
+bcftools view $4.bcf | vcfutils.pl varFilter -D100 > $4.vcf
 ###	Generate the .SNPS file
-python SNP_call.py $5.vcf $5.sort.bam $5.SNPS
+python SNP_call.py $4.vcf $4.sort.bam $4.SNPS
 
