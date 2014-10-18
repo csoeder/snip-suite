2553d1dd86753a51eb34d30d7332014eca663074
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-17 23:46:30 -0400

    oops fixed sampe

--- VCF_prepper.sh
+++ VCF_prepper.sh
@@ -13,7 +13,7 @@ module load samtools						#
 ###	Align the reads! ########################################
 bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $4_1.sai	#	Parameters from PSISeq procedure
 bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $4_2.sai	#
-bwa sampe $1 read1.sai read2.sai $2 $3 > $4.sam 			#
+bwa sampe $1 $4_1.sai $4_2.sai $2 $3 > $4.sam 			#
 ###	Cleanup the alignment	#################################
 samtools view -bS $4.sam > $4.bam							#
 samtools sort $4.bam $4.sort								#
