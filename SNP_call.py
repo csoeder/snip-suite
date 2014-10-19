18aa4803d3fd3a1bbfd286c6602f77c083b1f77c
Charles Soeder <Charles.Soeder@gmail.com>
2014-10-18 20:12:18 -0400

    turn off nonsense

--- SNP_call.py
+++ SNP_call.py
@@ -22,20 +22,20 @@ for rec in parser:
 #																		#if it's a SNP and it's called by more than 75% of the reads, and there are 12+ reads covering.
 		#																		Sekelsky data are monoploid, so no heterozygosity worries.
 
-		eff = open('%s.bed'%SNP_log, 'w')
-		eff.write('%s\t%s\t%s\n'%tuple([rec.CHROM, int(rec.POS), int(rec.POS)+1]))
-		eff.close()
-		coverage = float(os.system("bedtools coverage -abam %s -b %s.bed | cut -f 5 "%tuple([parental_bam, SNP_log])))	#	Site coverage
-		os.remove('%s.bed'%SNP_log)	#	cleanup
+		#eff = open('%s.bed'%SNP_log, 'w')
+		#eff.write('%s\t%s\t%s\n'%tuple([rec.CHROM, int(rec.POS), int(rec.POS)+1]))
+		#eff.close()
+		#coverage = float(os.system("bedtools coverage -abam %s -b %s.bed | cut -f 5 "%tuple([parental_bam, SNP_log])))	#	Site coverage
+		#os.remove('%s.bed'%SNP_log)	#	cleanup
 		#					^^^ This pulls the coverage at the SNP site
-		eff = open('%s.bed'%SNP_log, 'w')
-		eff.write('%s\t%s\t%s\n'%tuple([rec.CHROM, max(0, int(rec.POS)-1000), int(rec.POS)+1000]))
-		eff.close()
-		reg_cov = float(os.system("bedtools coverage -abam %s -b %s.bed | cut -f 5 | awk '{ total += $1; count++ } END { print total/count }' "%tuple([parental_bam, SNP_log])))	#	Regional coverage
+		#eff = open('%s.bed'%SNP_log, 'w')
+		#eff.write('%s\t%s\t%s\n'%tuple([rec.CHROM, max(0, int(rec.POS)-1000), int(rec.POS)+1000]))
+		#eff.close()
+		#reg_cov = float(os.system("bedtools coverage -abam %s -b %s.bed | cut -f 5 | awk '{ total += $1; count++ } END { print total/count }' "%tuple([parental_bam, SNP_log])))	#	Regional coverage
 		#					^^^	This pulls out average regional coverage to compare
-		os.remove('%s.bed'%SNP_log)	#	cleanup
-		#coverage = 0
-		#reg_cov = 0
+		#os.remove('%s.bed'%SNP_log)	#	cleanup
+		coverage = 0
+		reg_cov = 0
 		phial.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'%tuple([rec.CHROM, rec.POS, rec.REF, '%s'*len(rec.ALT)%tuple(rec.ALT), coverage, reg_cov, rec.INFO['DP'], rec.QUAL]))
 		#			Writes a flatfile of approved SNPs:	Chromome	Position	Reference	Alternate alleles	Coverage at-site 	Covereage +/- 1kb
 
