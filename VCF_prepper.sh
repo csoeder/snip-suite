9f1418831b6486201871753ab8bb1e4a09c71d8c
Charles Soeder <Charles.Soeder@gmail.com>
2014-11-03 11:27:56 -0500

    rewrote VCFprep for unpaired reads

--- VCF_prepper.sh
+++ VCF_prepper.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #############################################
 #	$1	the reference genome				#
-#	$2 										#
-#	$3	the FASTQ files of sequences to align
-#	$4	job title							#
+#	$2	the FASTQ files of sequences to align
+#	$3	job title							#
 #		THIS VERSION OPTIMIZED FOR RICH M.'S SUGARFLIES
 #############################################
 Load relevant modules 						#
@@ -12,16 +11,15 @@ module load bwa								#
 module load samtools						#
 #	Is the reference genome ready?			#
 ###	Align the reads! ########################################
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $4_1.sai	#	Parameters from PSISeq procedure
-bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $3 > $4_2.sai	#
-bwa sampe $1 $4_1.sai $4_2.sai $2 $3 > $4.sam 			#
+bwa aln -O 10 -E 3 -M 2 -o 2 -e 10 -n 5 $1 $2 > $3.sai	#	Parameters from PSISeq procedure
+bwa samse $1 $3.sai $2> $3.sam 			#
 ###	Cleanup the alignment	#################################
-samtools view -bS $4.sam > $4.bam							#
-samtools sort $4.bam $4.sort								#
-samtools index $4.sort.bam									#
+samtools view -bS $3.sam > $3.bam							#
+samtools sort $3.bam $3.sort								#
+samtools index $3.sort.bam									#
 ###	Generate the VCF ########################################
-samtools mpileup -uf $1 $4.sort.bam | bcftools view -bvcg - > $4.bcf
-bcftools view $4.bcf | vcfutils.pl varFilter -D100 > $4.vcf #	http://samtools.sourceforge.net/mpileup.shtml
+samtools mpileup -uf $1 $3.sort.bam | bcftools view -bvcg - > $3.bcf
+bcftools view $3.bcf | vcfutils.pl varFilter -D100 > $3.vcf #	http://samtools.sourceforge.net/mpileup.shtml
 ###	Generate the .SNPS file #################################
 #python SNP_call.py $4.vcf $4.sort.bam $4.SNPS				#
 #############################################################
