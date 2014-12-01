084a6ebf86f790f04d76cb781d074488a6e8c80e
Charles Soeder <Charles.Soeder@gmail.com>
2014-12-01 15:36:22 -0500

    urf... bundling operations within for loop, so input files don't just get repeatedly overwritten and the last one only bundled

--- moving_pictures.sh
+++ moving_pictures.sh
@@ -21,16 +21,20 @@ title_hyb=$(echo $3 | cut -f 1 -d . | rev | cut -f 1 -d / | rev)
 declare -a somes=("2L" "2R" "3L" "3R" "4" "X" "YHet" "2RHet")
 
 
+
+echo track name=$title_hyb description=$title_hyb itemRgb="On" > $title_hyb.bed
+
 for chrom in "${somes[@]}"; 
 
 	do echo $chrom;
 	python snip-suite/Portrayal_allSNPs.py $1 $title_1 $2 $title_2 $3 $title_hyb $4 $chrom; 
+
+	sed -e 's/^/chr/' *in_$title_hyb* >> $title_hyb.pre
+	sort -k 1,1 -k2,2n $title_hyb.pre >> $title_hyb.bed
+	rm $title_hyb.pre
+	rm *in_$title_hyb*
 	
 done
 
-echo track name=$title_hyb description=$title_hyb itemRgb="On" > $title_hyb.bed
 
-sed -e 's/^/chr/' *in_$title_hyb* >> $title_hyb.pre
-sort -k 1,1 -k2,2n $title_hyb.pre >> $title_hyb.bed
-rm $title_hyb.pre
-rm *in_$title_hyb*
+
