d6b31bf0ee7584825cc1961fc3eab451bcb118a3
Charles Soeder <Charles.Soeder@gmail.com>
2015-01-23 11:43:41 -0500

    works with BEDfiles for simplicity

--- bad_match.py
+++ bad_match.py
@@ -6,24 +6,21 @@ import csv
 import sys
 import re
 
-SAM_in = sys.argv[1]
-SAM_out = sys.argv[2]
-thresh = 1.0/3
+BED_in = sys.argv[1]
+BED_out = sys.argv[2]
+thresh = 0.25
 
 
-phial = open(SAM_out, 'w')
+phial = open(BED_out, 'w')
 
-with open(SAM_in, 'rb') as csvfile:
+with open(BED_in, 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter='\t')
 		for row in spamreader:
-			if row[0][0] == '@':	#	rewrite header lines
-				phial.write('%s\n'% tuple(['%s\t'*len(row)%tuple(row)]))	
-			else:
-				seq_len = float(len(row[9]))
-				cigar = row[5]
-				clips = sum(map(int, re.findall(r"([0-9]+)[SH]", cigar)))
-				if clips/seq_len > thresh:
-					phial.write('%s\n'% tuple(['%s\t'*len(row)%tuple(row)]))
+			cigar = row[6]
+			seq_len = float(sum(map(int, re.findall(r"([0-9]+)[MISX=]", cigar))))
+			clips = sum(map(int, re.findall(r"([0-9]+)[SH]", cigar)))
+			if clips/seq_len > thresh:
+				phial.write('%s\n'% tuple(['%s\t'*len(row)%tuple(row)]))
 
 phial.close()
 
