b9830bdd22b12a6ff109dffb49c9c0b83648c652
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-12 14:47:51 -0500

    rich's data are single-end reads

--- VCF_prepper_GSNAP.sh
+++ VCF_prepper_GSNAP.sh
@@ -2,14 +2,13 @@
 
 ref_genome=$1
 genome_index=$2 	#	cdjones_lab/csoeder/rich_introgression/ref_genomes/GMAP_ref/
-paired_end_1=$3
-paired_end_2=$4
-file_out=$5
+single_end_1=$3
+file_out=$4
 
 #	re: building GMAP index:	gmap_build -d dmel-all-chromosome-r5.57 -D GMAP_ref dmel-all-chromosome-r5.57.fasta	
 #	re: -d option ...	GSNAP seems to insist the the GMAP index be referenced relative to /proj/seq/data/gsnap/reference_genomes/ 
 
-gsnap -A sam -d ../../../$genome_index $paired_end_1 $paired_end_2 > "$file_out".sam
+gsnap -A sam -d ../../../$genome_index $single > "$file_out".sam
 
 samtools view -bS "$file_out".sam | samtools sort - "$file_out".sort								#
 samtools index "$file_out".sort.bam	
