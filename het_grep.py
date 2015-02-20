bf78061171035be3c6b8cb5e8b706ba5b9def47c
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-20 11:25:31 -0500

    het_grep uses DP4 rather than DP; more histogram scripts

--- het_grep.py
+++ het_grep.py
@@ -20,17 +20,36 @@ try:
 except IndexError:
 	cov_thresh = 10
 
+while False:
+	phial = open(file_out, 'w')
+	parser = vcf.Reader(open(file_in, 'r'))
+	for rec in parser:	#	For each record in the VCF...
+		if rec.is_snp and float(sum(rec.INFO['DP4'])) > cov_thresh:	#	if it records a SNP and is of sufficient depth...
+			fraction_nonref = float(sum(rec.INFO['DP4'][-2:]))/sum(rec.INFO['DP4'])	#	calculate the fraction of reads supporting the non-ref alleles
+			if fraction_nonref > lower_bound and fraction_nonref < upper_bound :
+				phial.write('%s\t%s\t%s\n'%tuple([rec.CHROM, rec.POS, rec.POS+1]))
+	phial.close()
 
+
+###############	Depict the 'could-be' heterozygotic AFs #######################
 phial = open(file_out, 'w')
 parser = vcf.Reader(open(file_in, 'r'))
+agreement = []
+total_num=0
 for rec in parser:	#	For each record in the VCF...
-	if rec.is_snp and float(rec.INFO['DP']) > cov_thresh:	#	if it records a SNP and is of sufficient depth...
+	if rec.is_snp and float(sum(rec.INFO['DP4'])) > cov_thresh:	#	if it records a SNP and is of sufficient depth...
+		total_num += 1
 		fraction_nonref = float(sum(rec.INFO['DP4'][-2:]))/sum(rec.INFO['DP4'])	#	calculate the fraction of reads supporting the non-ref alleles
-		if fraction_nonref > lower_bound and fraction_nonref < upper_bound :
-			phial.write('%s\t%s\t%s\n'%tuple([rec.CHROM, rec.POS, rec.POS+1]))
-
-
+		if fraction_nonref > 0.1 and fraction_nonref < 0.9 :	#	If the reads 
+			agreement.append(fraction_nonref)
 phial.close()
+
+plt.hist(agreement, bins=18)
+plt.title('Non-Homozygous Non-Reference Allele Frequencies\n(%s of %s Total Called SNPs)'%tuple([len(agreement), total_num]))
+plt.xlabel('Fraction of Quality Reads Supporting a Non-Ref')
+plt.ylabel('Fraction of SNPs with Measured Frequency')
+plt.savefig('heterozygous_histogram.png')
+
 
 
 # het_dict = {}
