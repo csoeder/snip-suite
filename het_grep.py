fd9d39fd155feebb08d4f468ce983ceffd37d894
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-20 12:05:53 -0500

    more plotting etc

--- het_grep.py
+++ het_grep.py
@@ -20,35 +20,37 @@ try:
 except IndexError:
 	cov_thresh = 10
 
-while False:
-	phial = open(file_out, 'w')
-	parser = vcf.Reader(open(file_in, 'r'))
-	for rec in parser:	#	For each record in the VCF...
-		if rec.is_snp and float(sum(rec.INFO['DP4'])) > cov_thresh:	#	if it records a SNP and is of sufficient depth...
-			fraction_nonref = float(sum(rec.INFO['DP4'][-2:]))/sum(rec.INFO['DP4'])	#	calculate the fraction of reads supporting the non-ref alleles
-			if fraction_nonref > lower_bound and fraction_nonref < upper_bound :
-				phial.write('%s\t%s\t%s\n'%tuple([rec.CHROM, rec.POS, rec.POS+1]))
-	phial.close()
-
-
-###############	Depict the 'could-be' heterozygotic AFs #######################
 phial = open(file_out, 'w')
-parser = vcf.Reader(open(file_in, 'r'))
 agreement = []
+locations = []
 total_num=0
+parser = vcf.Reader(open(file_in, 'r'))
 for rec in parser:	#	For each record in the VCF...
 	if rec.is_snp and float(sum(rec.INFO['DP4'])) > cov_thresh:	#	if it records a SNP and is of sufficient depth...
 		total_num += 1
 		fraction_nonref = float(sum(rec.INFO['DP4'][-2:]))/sum(rec.INFO['DP4'])	#	calculate the fraction of reads supporting the non-ref alleles
-		if fraction_nonref > 0.1 and fraction_nonref < 0.9 :	#	If the reads 
+		if fraction_nonref > lower_bound and fraction_nonref < upper_bound :
+			phial.write('%s\t%s\t%s\n'%tuple([rec.CHROM, rec.POS, rec.POS+1]))
 			agreement.append(fraction_nonref)
+			locations.append(rec.CHROM)
 phial.close()
+		
+locations = np.array(locations)
+loc_dict={}
+for loc in np.unique(locations):
+	loc_dict[loc] = len(locations[locations == loc])
+
 
 plt.hist(agreement, bins=18)
+plt.subplot(211)
 plt.title('Non-Homozygous Non-Reference Allele Frequencies\n(%s of %s Total Called SNPs)'%tuple([len(agreement), total_num]))
 plt.xlabel('Fraction of Quality Reads Supporting a Non-Ref')
-plt.ylabel('Fraction of SNPs with Measured Frequency')
-plt.savefig('heterozygous_histogram.png')
+plt.ylabel('Number of SNPs with Measured Frequency')
+plt.subplot(212)
+plt.pie(loc_dict.values(), label=loc_dict.keys())
+plt.legend()
+plt.title('Non-Homozygous Non-Reference Alleles by Chromosome')
+plt.savefig('heterozygous_stats.png')
 
 
 
