3761c535f963bb594bec9b9cb5f9b17bb463aca1
Charles Soeder <Charles.Soeder@gmail.com>
2015-02-22 18:52:36 -0500

    proper arguments

--- VCF_prepper_GSNAP.sh
+++ VCF_prepper_GSNAP.sh
@@ -9,12 +9,12 @@ file_out=$5
 #	re: building GMAP index:	gmap_build -d dmel-all-chromosome-r5.57 -D GMAP_ref dmel-all-chromosome-r5.57.fasta	
 #	re: -d option ...	GSNAP seems to insist the the GMAP index be referenced relative to /proj/seq/data/gsnap/reference_genomes/ 
 
-gsnap -A sam -d ../../../ $paired_end_1 $paired_end_2 > "$file_out".sam
+gsnap -A sam -d ../../../"$genome_index" "$paired_end_1" "$paired_end_2" > "$file_out".sam
 
 samtools view -bS "$file_out".sam | samtools sort - "$file_out".sort								#
 samtools index "$file_out".sort.bam	
 
-samtools mpileup -uf $ref_genome "$file_out".sort.bam | bcftools view -bvcg - | bcftools view - | vcfutils.pl varFilter -D100 > "$file_out".vcf
+samtools mpileup -uf "$ref_genome" "$file_out".sort.bam | bcftools view -bvcg - | bcftools view - | vcfutils.pl varFilter -D100 > "$file_out".vcf
 
 
 
